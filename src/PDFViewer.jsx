54395c4d1efa29394c576695b55784c255c58ed6
Jacquelyn <jaxlynhyde@gmail.com>
2023-06-03 16:45:14 -0400

    add iframe

--- src/PDFViewer.jsx
+++ src/PDFViewer.jsx
@@ -3,7 +3,7 @@ import resume from './resume.pdf'
 
 const PDFViewer = () => {
   return (
-      <embed className='resume' src={resume} title="Resume" />
+      <iframe className='resume' src={resume} title="Resume" />
   );
 };
 
