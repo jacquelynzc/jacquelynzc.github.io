787572608775d51e7cd1e87b2707c0604859e234
Jacquelyn <jaxlynhyde@gmail.com>
2023-06-08 21:16:16 -0400

    updated tool icons

--- src/Tools.jsx
+++ src/Tools.jsx
@@ -15,20 +15,20 @@ const Tools = (props) => {
       <div className="progress-container">
         <div>
           <DevIcon icon="javascript" style={devIconStyle} viewBox="0 0 32 32" />
-          <DevIcon icon="react" style={devIconStyle} viewBox="0 0 32 32" />
           <DevIcon icon="python" style={devIconStyle} viewBox="0 0 32 32" />
           <DevIcon icon="git" style={devIconStyle} viewBox="0 0 32 32" />
           <DevIcon icon="postgresql" style={devIconStyle} viewBox="0 0 32 32" />
+          <DevIcon icon="npm" style={devIconStyle} viewBox="0 0 32 32" />
           <DevIcon icon="css3" style={devIconStyle} viewBox="0 0 32 32" />
           <DevIcon icon="html5" style={devIconStyle} viewBox="0 0 32 32" />
           <DevIcon icon="django" style={devIconStyle} viewBox="0 0 32 32" />
-  
-          <DevIcon icon="vim" style={devIconStyle} viewBox="0 0 32 32" />
-          <DevIcon icon="npm" style={devIconStyle} viewBox="0 0 32 32" />
-          <DevIcon icon="mongodb" style={devIconStyle} viewBox="0 0 32 32" />
-          <DevIcon icon="heroku" style={devIconStyle} viewBox="0 0 32 32" />
           <DevIcon icon="ruby" style={devIconStyle} viewBox="0 0 32 32" />
+  
           <DevIcon icon="nodejs" style={devIconStyle} viewBox="0 0 32 32" />
+          <DevIcon icon="mongodb" style={devIconStyle} viewBox="0 0 32 32" />
+          <DevIcon icon="vim" style={devIconStyle} viewBox="0 0 32 32" />
+          <DevIcon icon="react" style={devIconStyle} viewBox="0 0 32 32" />
+          <DevIcon icon="heroku" style={devIconStyle} viewBox="0 0 32 32" />
           <DevIcon icon="wordpress" style={devIconStyle} viewBox="0 0 32 32" />
           <DevIcon icon="photoshop" style={devIconStyle} viewBox="0 0 32 32" />
         </div>
